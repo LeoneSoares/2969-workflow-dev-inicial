21f0fc5ba77540ae0b7cccf82e04c13b56bb8aba
LeoneSoares <132823609+LeoneSoares@users.noreply.github.com>
2023-05-28 12:46:00 -0300

    fix: :bug: fix: erro no tratamento de exceção do método listarEventos

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -7,7 +7,7 @@ class EventosController{
             return res.status(200).json(resultado);
         }
         catch (err) {
-            return res.status(500).json(resultado);
+            return res.status(500).json(err.message);
         }
     }
 }
